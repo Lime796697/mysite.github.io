85501e65199f63c60ac8ddf97fe51a1551dd16d7
Nick <45765198+Lime796697@users.noreply.github.com>
2024-12-14 20:27:58 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const contactButton = document.getElementById('contactButton');
+    contactButton.addEventListener('click', function() {
+      window.location.href = 'https://vk.com/ijotarro';
+    });
+});
